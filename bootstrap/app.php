5cda479359a2ce1a7cc644a9a0246594d75c047c
maulanaafandi <maulanaafandi0005@gmail.com>
2026-03-23 10:14:47 +0700

    modified

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'is_pengurus' => \App\Http\Middleware\CheckIsPengurus::class,
+            'is_admin' => \App\Http\Middleware\CheckIsAdmin::class,
+        ]);
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })

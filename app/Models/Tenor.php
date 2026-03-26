d11b80566178bcad655fbbbc38fadb5bb5d69904
maulanaafandi <maulanaafandi0005@gmail.com>
2026-03-26 07:13:48 +0700

    modified model

---
+++ app/Models/Tenor.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Tenor extends Model
+{
+    protected $table = 'tenor';
+    protected $fillable = [
+        'tenor',
+        'persen'
+        ];
+    public $timestamps = false;
+    /**
+     * Relasi ke model Pinjaman.
+     * Satu tenor bisa digunakan oleh banyak data pinjaman.
+     */
+    public function pinjaman(): HasMany
+    {
+        return $this->hasMany(Pinjaman::class, 'id_tenor');
+    }
+}

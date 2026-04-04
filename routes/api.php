927867526fa7ca15e1ad9002ccfdbdaebf08c10e
maulanaafandi <maulanaafandi0005@gmail.com>
2026-04-04 12:57:11 +0700

    update api

--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,27 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 Route::post('/admin/login', [AuthController::class, 'loginAdmin']);
-Route::post('/pengurus/register', [AuthController::class, 'registerPengurus']);
 Route::post('/pengurus/login', [AuthController::class, 'loginPengurus']);
+Route::post('/daftar-ulang-pengurus', [AuthController::class, 'daftarUlangPengurus']);
 Route::post('/anggota/register', [AuthController::class, 'registerAnggota']);
 Route::post('/anggota/login', [AuthController::class, 'loginAnggota']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
     Route::get('/admin/pengurus', [PengurusManagementController::class, 'index']);
-    Route::get('/admin/detail-pengurus/{id}', [PengurusManagementController::class, 'show']);
-    Route::post('/admin/pengurus', [PengurusManagementController::class, 'store']);
+    Route::get('/admin/detail-akun-pengurus/{id}', [PengurusManagementController::class, 'show']);
+    Route::post('/admin/buat-nomor-pengurus', [PengurusManagementController::class, 'store']);
     Route::patch('admin/update-status-pengurus/{id}', [PengurusManagementController::class, 'updateStatus']);
     Route::patch('/admin/reset-password-pengurus/{id}', [PengurusManagementController::class, 'resetPassword']);
 
     Route::get('/admin/jenis-simpanan', [JenisSimpananController::class, 'index']);
-    Route::post('/admin/jenis-simpanan', [JenisSimpananController::class, 'store']);
+    Route::post('/admin/buat-jenis-simpanan', [JenisSimpananController::class, 'store']);
     Route::get('/admin/jenis-simpanan/{id}', [JenisSimpananController::class, 'show']);
     Route::patch('/admin/update-jenis-simpanan/{id}', [JenisSimpananController::class, 'update']);
 
     Route::get('/admin/lama-angsuran', [LamaAngsuranController::class, 'index']);
-    Route::post('/admin/lama-angsuran', [LamaAngsuranController::class, 'store']);
-    Route::get('/admin/lama-angsuran/{id}', [LamaAngsuranController::class, 'show']);
+    Route::post('/admin/buat-lama-angsuran', [LamaAngsuranController::class, 'store']);
+    Route::get('/admin/detail-lama-angsuran/{id}', [LamaAngsuranController::class, 'show']);
     Route::patch('/admin/update-lama-angsuran/{id}', [LamaAngsuranController::class, 'update']);
 });
 

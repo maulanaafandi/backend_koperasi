b1a9128169aba341955cae7c5ad3f8c0f958448a
maulanaafandi <maulanaafandi0005@gmail.com>
2026-04-10 07:36:14 +0700

    add midddleware anggota

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'is_pengurus' => \App\Http\Middleware\CheckIsPengurus::class,
             'is_admin' => \App\Http\Middleware\CheckIsAdmin::class,
+            'is_anggota' => \App\Http\Middleware\CheckIsAnggota::class,
         ]);
     })
     ->withMiddleware(function (Middleware $middleware): void {

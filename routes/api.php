4440ad304dd852224261cfed559576b5b5f33c36
maulanaafandi <maulanaafandi0005@gmail.com>
2026-04-20 19:09:03 +0700

    new api

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\DashboardAdminController;
 use App\Http\Controllers\Admin\PengurusManagementController;
+use App\Http\Controllers\Admin\NasabahController;
 use App\Http\Controllers\Admin\JenisSimpananController;
 use App\Http\Controllers\Admin\LamaAngsuranController;
-use App\Http\Controllers\Pengurus\AnggotaManagementController;
-use App\Http\Controllers\Pengurus\PinjamanController;
+use App\Http\Controllers\Admin\PinjamanController;
+use App\Http\Controllers\Pengurus\DashboardPengurusController;
+use App\Http\Controllers\Pengurus\NasabahManagementController;
+use App\Http\Controllers\Pengurus\TransaksiSimpananController;
+use App\Http\Controllers\Pengurus\TransaksiTarikController;
+use App\Http\Controllers\Pengurus\TransaksiPinjamanController;
+use App\Http\Controllers\Pengurus\PengumumanController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,39 +23,70 @@ Route::get('/user', function (Request $request) {
 Route::post('/admin/login', [AuthController::class, 'loginAdmin']);
 Route::post('/pengurus/login', [AuthController::class, 'loginPengurus']);
 Route::post('/daftar-ulang-pengurus', [AuthController::class, 'daftarUlangPengurus']);
-Route::post('/anggota/register', [AuthController::class, 'registerAnggota']);
-Route::post('/anggota/login', [AuthController::class, 'loginAnggota']);
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware(['auth:sanctum', 'is_admin'])->group(function () {
+    Route::get('/admin/dashboard', [DashboardAdminController::class, 'admin']);
     Route::get('/admin/pengurus', [PengurusManagementController::class, 'index']);
+    Route::get('/admin/get-nasabah', [NasabahController::class, 'indexNasabah']);
+    Route::get('/admin/detail-nasabah/{id}', [NasabahController::class, 'detailNasabah']);
     Route::get('/admin/detail-akun-pengurus/{id}', [PengurusManagementController::class, 'show']);
     Route::post('/admin/buat-nomor-pengurus', [PengurusManagementController::class, 'store']);
     Route::patch('admin/update-status-pengurus/{id}', [PengurusManagementController::class, 'updateStatus']);
-    Route::patch('/admin/reset-password-pengurus/{id}', [PengurusManagementController::class, 'resetPassword']);
+    Route::patch('admin/reset-password-pengurus/{id}', [PengurusManagementController::class, 'resetPassword']);
 
     Route::get('/admin/jenis-simpanan', [JenisSimpananController::class, 'index']);
     Route::post('/admin/buat-jenis-simpanan', [JenisSimpananController::class, 'store']);
-    Route::get('/admin/jenis-simpanan/{id}', [JenisSimpananController::class, 'show']);
+    Route::get('/admin/detail-jenis-simpanan/{id}', [JenisSimpananController::class, 'show']);
+    Route::get('/admin/get-jenis-simpanan/{id}', [JenisSimpananController::class, 'getById']);
     Route::patch('/admin/update-jenis-simpanan/{id}', [JenisSimpananController::class, 'update']);
 
     Route::get('/admin/lama-angsuran', [LamaAngsuranController::class, 'index']);
     Route::post('/admin/buat-lama-angsuran', [LamaAngsuranController::class, 'store']);
     Route::get('/admin/detail-lama-angsuran/{id}', [LamaAngsuranController::class, 'show']);
+    Route::get('/admin/get-lama-angsuran/{id}', [LamaAngsuranController::class, 'getById']);
     Route::patch('/admin/update-lama-angsuran/{id}', [LamaAngsuranController::class, 'update']);
+    Route::delete('/admin/hapus-lama-angsuran/{id}', [LamaAngsuranController::class, 'destroy']);
+
+    Route::get('/admin/pinjaman-pengajuan', [PinjamanController::class, 'pengajuan']);
+    Route::get('/admin/detail-pinjaman-pengajuan/{id}', [PinjamanController::class, 'detailPengajuan']);
+    Route::get('/admin/pinjaman-jatuh-tempo', [PinjamanController::class, 'jatuhTempo']);
+    Route::get('/admin/detail-pinjaman-jatuh-tempo/{id}', [PinjamanController::class, 'detailJatuhTempo']);
+    Route::get('/admin/pinjaman-macet', [PinjamanController::class, 'macet']);
+    Route::get('/admin/detail-pinjaman-macet/{id}', [PinjamanController::class, 'detailMacet']);
+    Route::get('/admin/pinjaman-lunas', [PinjamanController::class, 'lunas']);
+    Route::get('/admin/detail-pinjaman-lunas/{id}', [PinjamanController::class, 'detailLunas']);
+
 });
 
 Route::middleware(['auth:sanctum', 'is_pengurus'])->group(function () {
-    
-    // Route::get('/pengurus/akun-anggota', [AnggotaManagementController::class, 'index']);
-    // Route::get('/pengurus/detail-akun-anggota/{id}', [AnggotaManagementController::class, 'show']);
-    // Route::put('/pengurus/update-status-anggota/{id}', [AnggotaManagementController::class, 'updateStatus']);
-    // Route::delete('/pengurus/hapus-anggota/{id}', [AnggotaManagementController::class, 'destroy']);
+    Route::get('/pengurus/dashboard', [DashboardPengurusController::class, 'index']);
+    Route::get('/pengurus/nasabah', [NasabahManagementController::class, 'index']);
+    Route::get('/pengurus/detail-akun-nasabah/{id}', [NasabahManagementController::class, 'show']);
+    Route::get('/pengurus/get-status-akun-nasabah/{id}', [NasabahManagementController::class, 'getStatus']);
+    Route::post('/pengurus/buat-nasabah', [NasabahManagementController::class, 'store']);
+    Route::patch('/pengurus/update-status-akun-nasabah/{id}', [NasabahManagementController::class, 'updateStatus']);
 
-    // Route::get('/pengurus/pinjaman', [PinjamanController::class, 'index']);
-    // Route::post('/pengurus/pinjaman', [PinjamanController::class, 'store']);
-    // Route::put('/pengurus/pinjaman/approve/{id}', [PinjamanController::class, 'approve']);
-    // Route::delete('/pengurus/pinjaman/{id}', [PinjamanController::class, 'destroy']);
-    
+    Route::post('/pengurus/buat-transaksi-simpanan', [TransaksiSimpananController::class, 'simpan']);
+    Route::get('/pengurus/transaksi-simpanan', [TransaksiSimpananController::class, 'indexSimpan']);
+    Route::get('/pengurus/detail-transaksi-simpanan/{id}', [TransaksiSimpananController::class, 'detailSimpan']);
+
+    Route::post('/pengurus/buat-transaksi-tarik', [TransaksiTarikController::class, 'tarik']);
+    Route::get('/pengurus/transaksi-tarik', [TransaksiTarikController::class, 'indexTarik']);
+    Route::get('/pengurus/detail-transaksi-tarik/{id}', [TransaksiTarikController::class, 'detailTarik']);
+
+    Route::get('/pengurus/transaksi-pinjam', [TransaksiPinjamanController::class, 'index']);
+    Route::get('/pengurus/detail-transaksi-pinjam/{id}', [TransaksiPinjamanController::class, 'show']);
+    Route::get('/pengurus/status-transaksi-pinjam/{id}', [TransaksiPinjamanController::class, 'getStatus']);
+    Route::post('/pengurus/buat-transaksi-pinjam', [TransaksiPinjamanController::class, 'store']);
+    Route::patch('/pengurus/update-transaksi-pinjam/{id}', [TransaksiPinjamanController::class, 'approve']);
+
+    Route::get('/pengurus/pengumuman', [PengumumanController::class, 'index']);
+    Route::get('/pengurus/get-pengumuman/{id}', [PengumumanController::class, 'show']);
+    Route::get('/pengurus/detail-pengumuman/{id}', [PengumumanController::class, 'detailpengumuman']);
+    Route::post('/pengurus/buat-pengumuman', [PengumumanController::class, 'store']);
+    Route::patch('/pengurus/update-pengumuman/{id}', [PengumumanController::class, 'update']);
+    Route::post('/pengurus/update-foto-pengumuman/{id}', [PengumumanController::class, 'updateFoto']);
+    Route::delete('/pengurus/hapus-pengumuman/{id}', [PengumumanController::class, 'destroy']);
 });
 

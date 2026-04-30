966056ab67f5d03b0954437ded39c96f77e6cefa
maulanaafandi <maulanaafandi0005@gmail.com>
2026-04-30 18:54:49 +0700

    add

--- app/Http/Middleware/CheckOrigin.php
+++ app/Http/Middleware/CheckOrigin.php
@@ -9,16 +9,16 @@ use Symfony\Component\HttpFoundation\Response;
 class CheckOrigin
 {
 
-   public function handle($request, Closure $next)
-{
-    $allowedOrigin = 'http://192.168.1.7:8000';
+    public function handle($request, Closure $next)
+    {
+        $allowedOrigin = 'http://192.168.1.7:8000';
 
-    if ($request->headers->get('origin') !== $allowedOrigin) {
-        return response()->json([
-            'message' => 'Access denied.'
-        ], 403);
+        if ($request->headers->get('origin') !== $allowedOrigin) {
+            return response()->json([
+                'message' => 'Access denied.'
+            ], 403);
+        }
+
+        return $next($request);
     }
-
-    return $next($request);
-}
 }
